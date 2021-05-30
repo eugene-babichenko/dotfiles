0d4673c02a66c31269b67acf5fbc5ba4cbd3e283
Yevhenii Babichenko <eugene.babichenko@protonmail.com>
2021-05-30 17:14:04 +0300

    update fish shell configuration

--- fish/config.fish
+++ fish/config.fish
@@ -2,20 +2,17 @@ if not functions -q fisher
 	echo "Fisher is not installed. Run `install-fisher`."
 end
 
-# notify when a command ran for 15 seconds
-set -g __done_min_cmd_duration 15000
-
 # Make GnuPG work correctly
 set -gx GPG_TTY (tty)
 
 if type -q starship
-	starship init fish | source
+	starship init fish --print-full-init | source
 else
 	echo "Starship is not installed" 1>&2
 end
 
 if type -q thefuck
-	thefuck --alias | source
+	codegen-cache thefuck --alias | source
 else
 	echo "thefuck is not installed" 1>&2
 end
